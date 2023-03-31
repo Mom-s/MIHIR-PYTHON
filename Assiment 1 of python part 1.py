034d27c80b2cabbdf87014687a5f20eeca33e415
Moms <mihirrpatel14@gmail.com>
2023-03-31 11:06:02 -0400

    Changs made

--- Assiment 1 of python part 1.py
+++ Assiment 1 of python part 1.py
@@ -1,34 +1,34 @@
 #mihirs program
 def calculate_bill(cleaning, cavity_filling, x_ray):
-    subtotal = 0
+    Bill = 0
     if cleaning == 'y':
-        subtotal += 60
+        Bill += 60
     if cavity_filling == 'y':
-        subtotal += 200
+         Bill += 200
     if x_ray == 'y':
-        subtotal += 100
-    tax = subtotal * 0.15
-    total = subtotal + tax
-    if total > 300:
-        total *= 0.9
-    elif total > 200:
-        total *= 0.95
-    return total
+         Bill += 100
+    tax = Bill * 0.15
+    Total = Bill + tax
+    if  Total > 300:
+         Total *= 0.9
+    elif  Total > 200:
+         Total *= 0.95
+    return  Bill, Total ,tax
 
 name = input("Enter patient's name: ")
 cleaning = input("Was cleaning performed? (y/n): ")
 cavity_filling = input("Was cavity-filling performed? (y/n): ")
 x_ray = input("Was X-Ray performed? (y/n): ")
 
-total = calculate_bill(cleaning, cavity_filling, x_ray)
+Bill,total,tax = calculate_bill(cleaning, cavity_filling, x_ray)
 
 print("Melanie Dental Clinic")
 print("Patient name:", name)
 print("Cleaning:", cleaning)
 print("Cavity-filling:", cavity_filling)
 print("X-Ray:", x_ray)
-print("Subtotal:", '$' + str(subtotal))
-print("Tax:", '$' + str(tax))
-print("Total:", '$' + str(total))
+print("Subtotal:", str(Bill))
+print("Tax:", str(tax))
+print(total)
 
 
